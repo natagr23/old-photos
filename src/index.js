c2bd0078f58270d33b41b2d6fec5b287e584c4ca
natagr23 <nata_garcia23@hotmail.com>
2022-09-27 11:03:11 -0500

    adding meta  to google verification

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import GA4React from 'react-ga';
-
-// const ga4react = new GA4React('G-884HZWW40Y');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,13 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// (async () => {
-//   await ga4react.initialize();
-
-//   root.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   );
-// })();
